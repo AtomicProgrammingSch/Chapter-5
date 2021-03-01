5fd91e7b467bbbb3e84228f3e8d9c6a9605cb820
AtomicProgrammingSch <george-timms@chace.enfield.sch.uk>
2021-03-01 03:59:22 +0000

    Finished task-11

---
+++ task-11/main.py
@@ -0,0 +1,16 @@
+def magic_nunber():
+    number = 7
+    while True:
+        print("Please enter your guess:")
+        guess = int(input())
+        if guess > number:
+            print("Too high!")
+        elif guess < number:
+            print("Too low!")
+        else:
+            print("Well done!")
+            return
+
+
+if __name__ == "__main__":
+    magic_nunber()
